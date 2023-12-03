303dd25ff746a4fcb07f87e8576805b2f789be57
Juryn Tamtam <juryntamtam@gmail.com>
2023-12-03 20:22:56 +0800

    initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.example.affirmations
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Tips
+import com.example.affirmations.ui.theme.AppTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+
+fun MyApp() {
+    val tipsList = Datasource().loadTips()
+    Scaffold(
+        topBar = {
+            TipsTopAppBar()
+        },
+        content = {
+            TipList(
+               tipsList = tipsList,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 62.dp) // Adjust the padding as needed
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TipsTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_title),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        modifier = Modifier.statusBarsPadding()
+    )
+}
+
+@Composable
+fun TipList(tipsList: List<Tips>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(tipsList) { Tips ->
+            TipCard(
+                tip = Tips,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun TipCard(tip: Tips, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Text(
+                text = LocalContext.current.getString(tip.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Image(
+                painter = painterResource(tip.imageResourceId),
+                contentDescription = stringResource(tip.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(280.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = LocalContext.current.getString(tip.stringDescriptionResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TipCardPreview() {
+    TipCard(Tips(R.string.day1, R.drawable.image1, R.string.description1))
+}
---
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.affirmations.data
+import com.example.affirmations.R
+import com.example.affirmations.model.Tips
+
+/**
+ * [Datasource] generates a list of [Tips]
+ */
+class Datasource() {
+    fun loadTips(): List<Tips> {
+        return listOf<Tips>(
+
+            Tips(R.string.day1, R.drawable.image1, R.string.description1),
+            Tips(R.string.day2, R.drawable.image2, R.string.description2),
+            Tips(R.string.day3, R.drawable.image3, R.string.description3),
+            Tips(R.string.day4, R.drawable.image4, R.string.description4),
+            Tips(R.string.day5, R.drawable.image5, R.string.description5),
+            Tips(R.string.day6, R.drawable.image6, R.string.description6),
+            Tips(R.string.day7, R.drawable.image7, R.string.description7),
+            Tips(R.string.day8, R.drawable.image8, R.string.description8),
+            Tips(R.string.day9, R.drawable.image9, R.string.description9),
+            Tips(R.string.day10, R.drawable.image11, R.string.description10),
+            Tips(R.string.day11, R.drawable.image12, R.string.description11),
+            Tips(R.string.day12, R.drawable.image13, R.string.description12),
+            Tips(R.string.day13, R.drawable.image14, R.string.description13),
+            Tips(R.string.day14, R.drawable.image15, R.string.description14),
+            Tips(R.string.day15, R.drawable.image16, R.string.description15),
+            Tips(R.string.day16, R.drawable.image17, R.string.description16),
+            Tips(R.string.day17, R.drawable.image18, R.string.description17),
+            Tips(R.string.day18, R.drawable.image19, R.string.description18),
+            Tips(R.string.day19, R.drawable.image20, R.string.description19),
+            Tips(R.string.day20, R.drawable.image10, R.string.description20),
+            Tips(R.string.day21, R.drawable.image21, R.string.description21),
+            Tips(R.string.day22, R.drawable.image22, R.string.description22),
+            Tips(R.string.day23, R.drawable.image23, R.string.description23),
+            Tips(R.string.day24, R.drawable.image24, R.string.description24),
+            Tips(R.string.day25, R.drawable.image25, R.string.description25),
+            Tips(R.string.day26, R.drawable.image26, R.string.description26),
+            Tips(R.string.day27, R.drawable.image27, R.string.description27),
+            Tips(R.string.day28, R.drawable.image28, R.string.description28),
+            Tips(R.string.day29, R.drawable.image29, R.string.description29),
+            Tips(R.string.day30, R.drawable.image30, R.string.description30),
+            Tips(R.string.day31, R.drawable.last, R.string.description31)
+        )
+    }
+}
